38fe93aed0e15486c4f0009d7094eb523c311c77
OmidSargazi <om.sargazi@gmail.com>
2024-03-02 22:50:00 +0330

    create toggle menu button

---
+++ script.js
@@ -0,0 +1,6 @@
+// variables
+var settingsMenu = document.querySelector(".settings-menu");
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
+  console.log("object");
+}
